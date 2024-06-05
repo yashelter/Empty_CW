5587c8a5c45f083feca10769e0273056df19b33b
yashelter <yashelter@yandex.ru>
2024-06-05 15:50:06 +0300

    turned off disk controller, start debug

--- cursal_work/server/server/server.h
+++ cursal_work/server/server/server.h
@@ -1,7 +1,3 @@
-//
-// Created by Des Caldnd on 6/2/2024.
-//
-
 #ifndef MP_OS_SERVER_H
 #define MP_OS_SERVER_H
 
@@ -9,7 +5,7 @@
 #include <utility>
 #include <crow.h>
 #include <memory_controller.h>
-#include <disk_controller.h>
+//#include <disk_controller.h>
 #include <controller_int.h>
 
 template <serializable tkey, serializable tvalue>
@@ -24,5 +20,177 @@ public:
     Server(controller_int<tkey, tvalue>* controller, uint16_t port = 9300);
 };
 
+
+using json = nlohmann::json;
+
+
+template <
+        serializable tkey,
+        serializable tvalue>
+Server<tkey, tvalue>::Server(controller_int<tkey, tvalue>* controller, uint16_t port)
+        : _controller(controller)
+{
+    CROW_ROUTE(app, "/heart") ([&](const crow::request& req) {
+        return crow::response(200, "OK");
+    });
+
+    CROW_ROUTE(app, "/add_pool") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        CW_GUID result = _controller->add_pool(pool_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/remove_pool") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        CW_GUID result = _controller->remove_pool(pool_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/add_scheme") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        CW_GUID result = _controller->add_scheme(pool_name, scheme_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/remove_scheme") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        CW_GUID result = _controller->add_scheme(pool_name, scheme_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/add_collection") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        auto collection_name = req.url_params.get("collection_name");
+        CW_GUID result = _controller->add_collection(pool_name, scheme_name, collection_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/remove_collection") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        auto collection_name = req.url_params.get("collection_name");
+
+        CW_GUID result = _controller->remove_collection(pool_name, scheme_name, collection_name);
+
+        return crow::response(200, result.to_json());
+    });
+
+    CROW_ROUTE(app, "/insert") ([&](const crow::request &req)
+                                {
+                                    auto pool_name = req.url_params.get("pool_name");
+                                    auto scheme_name = req.url_params.get("scheme_name");
+                                    auto collection_name = req.url_params.get("collection_name");
+
+                                    auto data_json = req.url_params.get("data");
+
+                                    json data = json(data_json);
+
+                                    student value =  student::from_json(data);
+
+                                    CW_GUID result = _controller->insert(pool_name, scheme_name, collection_name, value._surname, value);
+
+                                    return crow::response(200, result.to_json());
+                                });
+
+
+    CROW_ROUTE(app, "/read_value") ([&](const crow::request &req)
+                                    {
+                                        auto pool_name = req.url_params.get("pool_name");
+                                        auto scheme_name = req.url_params.get("scheme_name");
+                                        auto collection_name = req.url_params.get("collection_name");
+                                        auto need_persist = req.url_params.get("need_persist");
+                                        auto time_str = req.url_params.get("time");
+                                        auto key = req.url_params.get("key");
+
+                                        typename controller_int<tkey, tvalue>::time_point_t time;
+                                        if (!time_str){
+                                            auto val = std::chrono::system_clock::from_time_t(std::stoll(time_str)); // TODO
+                                            time = val;
+                                        } else {
+                                            time = std::chrono::system_clock::now();
+                                        }
+
+                                        CW_GUID result = _controller->read_value(pool_name, scheme_name, collection_name, key, need_persist - '0', time);
+
+                                        return crow::response(200, result.to_json()); // TODO
+                                    });
+
+    CROW_ROUTE(app, "/read_range") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        auto collection_name = req.url_params.get("collection_name");
+        auto need_persist = req.url_params.get("need_persist");
+        auto time_str = req.url_params.get("time");
+
+        tkey lower_key = req.url_params.get("lower");
+        tkey upper_key = req.url_params.get("upper");
+        typename controller_int<tkey, tvalue>::time_point_t time;
+
+        if (!time_str)
+        {
+            time = std::chrono::system_clock::from_time_t(std::stoll(time_str)); // TODO
+        } else {
+            time = std::chrono::system_clock::now();
+        }
+
+        CW_GUID result = _controller->read_range(pool_name, scheme_name, collection_name, lower_key, upper_key, need_persist - '0', time);
+
+        return crow::response(200, result.to_json()); // TODO
+
+    });
+
+    CROW_ROUTE(app, "/update") ([&](const crow::request &req)
+                                {
+                                    auto pool_name = req.url_params.get("pool_name");
+                                    auto scheme_name = req.url_params.get("scheme_name");
+                                    auto collection_name = req.url_params.get("collection_name");
+
+                                    auto data_json = req.url_params.get("data");
+
+                                    json data = json(data_json);
+
+                                    student value =  student::from_json(data);
+
+                                    CW_GUID result = _controller->update(pool_name, scheme_name, collection_name, value._surname, value);
+
+                                    return crow::response(200, result.to_json());
+                                });
+
+    CROW_ROUTE(app, "/remove") ([&](const crow::request &req) {
+        auto pool_name = req.url_params.get("pool_name");
+        auto scheme_name = req.url_params.get("scheme_name");
+        auto collection_name = req.url_params.get("collection_name");
+
+        auto key = req.url_params.get("data");
+        CW_GUID result = _controller->remove(pool_name, scheme_name, collection_name, key);
+
+        return crow::response(200, result.to_json()); // TODO
+    });
+
+    CROW_ROUTE(app, "/get") ([&](const crow::request &req) {
+        auto body = req.url_params.get("GUID");
+        json jsn = json(body);
+        CW_GUID id = jsn;
+
+        std::optional<nlohmann::json> result = _controller->get(id);
+
+        if (result) {
+            return crow::response(200, result.value().dump());
+        } else {
+            return crow::response(404, "Not Found");
+        }
+    });
+
+    app.port(port).multithreaded();
+    app.run();
+}
 
 #endif //MP_OS_SERVER_H
